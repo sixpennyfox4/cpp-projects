b22b067d03c42ce445ee8f62836f381994175032
sixpennyfox4 <asenoprenov2022@gmail.com>
2024-12-31 15:21:46 +0200

    Create main.cpp

---
+++ Auto Clicker/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <Windows.h>
+
+int main() {
+	while (true) {
+		Sleep(50);
+
+		if (GetAsyncKeyState(VK_F6)) {
+			INPUT input = { 0 };
+			input.type = INPUT_MOUSE;
+			input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+			SendInput(1, &input, sizeof(input));
+
+			ZeroMemory(&input, sizeof(input));
+
+			input.type = INPUT_MOUSE;
+			input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+			SendInput(1, &input, sizeof(input));
+		}
+
+		if (GetAsyncKeyState(VK_F7)) {
+			INPUT input = { 0 };
+			input.type = INPUT_KEYBOARD;
+			input.ki.wVk = VkKeyScanA('d');
+			SendInput(1, &input, sizeof(input));
+
+			ZeroMemory(&input, sizeof(input));
+
+			input.ki.dwFlags = KEYEVENTF_KEYUP;
+			SendInput(1, &input, sizeof(input));
+		}
+	}
+
+	return 0;
+}
